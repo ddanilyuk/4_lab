9092b1a8e0f88ed5cbb9bedb1b138ed5d81f1dcd
donis <danis.danilyuk@gmail.com>
2019-03-03 00:53:22 +0200

    v_1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Cars my_car = new Cars("Porshe", "911", "black", 10, 20);
+        // TODO: 2019-03-03 add sorting to all
         my_car.setColour("white");
         System.out.println(my_car.getColour());
     }
